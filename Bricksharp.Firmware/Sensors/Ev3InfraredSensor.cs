8a27b66a5f3e1e47796ab107441669725ce15e11
Kirk Woll <kirk.woll@gmail.com>
2017-09-11 06:23:46 -0700

    misc

---
+++ Bricksharp.Firmware/Sensors/Ev3InfraredSensor.cs
@@ -0,0 +1,19 @@
+﻿using Bricksharp.Firmware.Classes;
+
+namespace Bricksharp.Firmware.Sensors
+{
+    public class Ev3InfraredSensor : Sensor
+    {
+        private EnumClassProperty<Ev3InfraredSensorMode> mode;
+
+        public Ev3InfraredSensor(int port) : base(port)
+        {
+            mode = new EnumClassProperty<Ev3InfraredSensorMode>(Folder, "mode");
+        }
+
+        protected override void OnTriggered(string value)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Bricksharp.Firmware/Sensors/Ev3InfraredSensorMode.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+
+namespace Bricksharp.Firmware.Sensors
+{
+    public enum Ev3InfraredSensorMode
+    {
+        [EnumMember(Value = "IR-PROX")]
+        Proximity,
+
+        [EnumMember(Value = "IR-SEEK")]
+        Seek,
+
+        [EnumMember(Value = "IR-REMOTE")]
+        Remote
+    }
+}
